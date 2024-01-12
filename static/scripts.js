c53459d30b35a57f176c8fbbf72066936bd4c0e0
Andrei Anton <softwebdev05@gmail.com>
2024-01-12 12:50:39 +0200

    Added communication between js and flask, random move response.

--- static/scripts.js
+++ static/scripts.js
@@ -3,7 +3,6 @@ var board,
   statusEl = $('#status'),
   fenEl = $('#fen'),
   pgnEl = $('#pgn');
-
 // do not pick up pieces if the game is over
 // only pick up pieces for the side to move
 var onDragStart = function(source, piece, position, orientation) {
@@ -13,7 +12,6 @@ var onDragStart = function(source, piece, position, orientation) {
     return false;
   }
 };
-
 var onDrop = function(source, target) {
   // see if the move is legal
   var move = game.move({
@@ -21,11 +19,11 @@ var onDrop = function(source, target) {
     to: target,
     promotion: 'q' // NOTE: always promote to a queen for example simplicity
   });
-
   // illegal move
   if (move === null) return 'snapback';
 
   updateStatus();
+  randomResponse();
 };
 
 // update the board position after the piece snap
@@ -42,36 +40,29 @@ var updateStatus = function() {
   if (game.turn() === 'b') {
     moveColor = 'Black';
   }
-
   // checkmate?
   if (game.in_checkmate() === true) {
     status = 'Game over, ' + moveColor + ' is in checkmate.';
   }
-
   // draw?
   else if (game.in_draw() === true) {
     status = 'Game over, drawn position';
   }
-
   // game still on
   else {
     status = moveColor + ' to move';
-
     // check?
     if (game.in_check() === true) {
       status += ', ' + moveColor + ' is in check';
     }
   }
-
   document.getElementById("status").innerHTML = status;
 //  document.getElementById("fen").innerHTML = game.fen();
   document.getElementById("pgn").innerHTML = game.pgn();
-
   statusEl.html(status);
   fenEl.html(game.fen());
   pgnEl.html(game.pgn());
 };
-
 var cfg = {
   draggable: true,
   position: 'start',
@@ -80,6 +71,20 @@ var cfg = {
   onSnapEnd: onSnapEnd
 };
 
+var randomResponse = function() {
+    fen = game.fen()
+    $.get($SCRIPT_ROOT + "/move/" + fen, function(data) {
+        game.move(data, {sloppy: true});
+//        board.position(game.fen());
+        updateStatus();
+    })
+}
+
+var testResponse = function(tester) {
+    $.get($SCRIPT_ROOT + "/move/" + tester, function(data) {
+        console.log(data);
+    })
+}
 
 // did this based on a stackoverflow answer
 // http://stackoverflow.com/questions/29493624/cant-display-board-whereas-the-id-is-same-when-i-use-chessboard-js
@@ -87,4 +92,3 @@ setTimeout(function() {
     board = ChessBoard('board', cfg);
     updateStatus();
 }, 0);
-
